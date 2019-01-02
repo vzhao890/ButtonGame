77c85ea05679d581b3996a87713d19a2c787f855
BT_1N3_22 <@>
2019-01-02 10:05:43 -0500

    1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller
+{
+    public Label points;
+    public int score=0;
+
+    long time = System.nanoTime();
+    public void addOne(ActionEvent actionEvent) {
+        if (timer() == 0) {
+            score++;
+            points.setText("Points: " + score);
+        }
+    }
+
+    public int timer()
+    {
+        double a=(double)(System.nanoTime() - time)/(100000000);
+        if(a==10.0) {
+            return -1;
+        }
+        return 0;
+    }
 }
