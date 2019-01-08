7797e044a419c857aa9b03b81178ac77d9a9cf15
BT_1N3_10 <@>
2019-01-08 08:58:55 -0500

    4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,7 +8,6 @@ public class Controller
 {
     public Label points;
     public int score=0;
-    public int time = 10;
     public boolean count = true;
 
     public void addOne(ActionEvent actionEvent) {
